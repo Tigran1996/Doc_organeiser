489b461015a97e80af344a939ec4a1f1b8ee779b
tiko <tiko@gmail.com>
2019-06-17 12:06:20 +0400

    Doc dashboard

---
+++ js/Dashboard.js
@@ -0,0 +1,10 @@
+
+// /*Menu-toggle*/
+// $("#menu-toggle").click(function(e) {
+//     e.preventDefault();
+//     $("#wrapper").toggleClass("active");
+// });
+//
+// /*Scroll Spy*/
+// $('body').scrollspy({ target: '#spy', offset:80});
+
--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ $(document).ready(function(){
 	);
 	var inp = $("#docs-demo");
 })
+
+
+
